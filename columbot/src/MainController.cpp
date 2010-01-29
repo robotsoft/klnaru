106811e9deaca813d092e76cb71683d09e9b90d8
soonhac.hong <soonhac.hong@75a8fc1a-f4b0-11dd-9d1d-173cc1d06e3c>
2010-01-29 22:23:32 +0000

    Add MainController.cpp and staubli service(staubli.srv).

--- columbot/src/MainController.cpp
+++ columbot/src/MainController.cpp
@@ -7,6 +7,8 @@
 
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <columbot/staubli.h>
+
 void chatterCallback(const std_msgs::StringConstPtr& msg)
 {
   ROS_INFO("Received [%s]", msg->data.c_str());
@@ -16,6 +18,30 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "MainController");
   ros::NodeHandle n;
-  ros::Subscriber chatter_sub = n.subscribe("chatter", 100, chatterCallback);
-  ros::spin();
+  //ros::Subscriber chatter_sub = n.subscribe("chatter", 100, chatterCallback);
+  //ros::spin();
+
+  ros::ServiceClient client = n.serviceClient<columbot::staubli>("staubli");
+  columbot::staubli staubli_srv;
+  staubli_srv.request.command = 1;
+  if (client.call(staubli_srv))
+  {
+    ROS_INFO("Staubli Postion x : %f", staubli_srv.response.x);
+    ROS_INFO("Staubli Postion y : %f", staubli_srv.response.y);
+    ROS_INFO("Staubli Postion z : %f", staubli_srv.response.z);
+    ROS_INFO("Staubli Postion theta_x : %f", staubli_srv.response.theta_x);
+    ROS_INFO("Staubli Postion theta_y : %f", staubli_srv.response.theta_y);
+    ROS_INFO("Staubli Postion theta_z : %f", staubli_srv.response.theta_z);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service staubli");
+    return 1;
+  }
+
+  //Got target position
+
+  //Move robot arm to the target position
+	  //send a message
+  //Grasp target
 }
