010ce7ede9ee9fd4887cfe4189f74043400c40f7
soonhac.hong <soonhac.hong@75a8fc1a-f4b0-11dd-9d1d-173cc1d06e3c>
2010-02-03 22:32:44 +0000

    Complete get position.

--- columbot/src/MainController.cpp
+++ columbot/src/MainController.cpp
@@ -7,7 +7,13 @@
 
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+//#include <staubli/command.h>
 #include <command.h>		//use message of staubli package; why didn't this file include remotely?
+
+
+#define PI 3.14
+#define DEGREE_2_RADIAN(X) (X*PI/180)
+#define RAIDAN_2_DEGREE(X) (X*180/PI)
 
 void chatterCallback(const std_msgs::StringConstPtr& msg)
 {
@@ -23,6 +29,8 @@ int main(int argc, char** argv)
 
   ros::ServiceClient client = n.serviceClient<staubli::command>("command");
   staubli::command staubli_srv;
+
+  //Get current cartesian postion
   staubli_srv.request.command_number = 1;
   if (client.call(staubli_srv))
   {
@@ -39,6 +47,34 @@ int main(int argc, char** argv)
     return 1;
   }
 
+  //Debug here !!
+  //Move joints
+  staubli_srv.request.command_number = 2;
+  staubli_srv.request.joint1 = DEGREE_2_RADIAN(54.18);
+  staubli_srv.request.joint2 = DEGREE_2_RADIAN(-30.31);
+  staubli_srv.request.joint3 = DEGREE_2_RADIAN(56.6);
+  staubli_srv.request.joint4 = DEGREE_2_RADIAN(-8.81);
+  staubli_srv.request.joint5 = DEGREE_2_RADIAN(-23.6);
+  staubli_srv.request.joint6 = DEGREE_2_RADIAN(-62.0);
+
+  if (client.call(staubli_srv))
+  {
+    ROS_INFO("Staubli joint 1 : %f", staubli_srv.response.joint1);
+    ROS_INFO("Staubli joint 2 : %f", staubli_srv.response.joint2);
+    ROS_INFO("Staubli joint 3 : %f", staubli_srv.response.joint3);
+    ROS_INFO("Staubli joint 4 : %f", staubli_srv.response.joint4);
+    ROS_INFO("Staubli joint 5 : %f", staubli_srv.response.joint5);
+    ROS_INFO("Staubli joint 6 : %f", staubli_srv.response.joint6);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service staubli");
+    return 1;
+  }
+
+
+
+  //Senario 1
   //Got target position
 
   //Move robot arm to the target position
