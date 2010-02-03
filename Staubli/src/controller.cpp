0f2408353370c6b546edf4675a6aa8cb0ac68bdd
soonhac.hong <soonhac.hong@75a8fc1a-f4b0-11dd-9d1d-173cc1d06e3c>
2010-02-03 22:33:15 +0000

    Complete in get position.

--- Staubli/src/controller.cpp
+++ Staubli/src/controller.cpp
@@ -21,27 +21,68 @@ void print(double e){
 
 int GetPosition(staubli::command::Response *res){
 
-	//TX60L robot;
-    //robot.Login("http://128.59.20.63:5653/", "default", ""); //ANTIGUA
+	TX60L robot;
+    robot.Login("http://128.59.20.63:5653/", "default", ""); //ANTIGUA
 	//robot.Login("http://128.59.19.61:5653/", "default", ""); //Thomas' computer
-	//robot.Power(true);
+	robot.Power(true);
+	ROS_INFO("Staubli is connected !!.");
 
-	//std::vector<double> position;
-	//position.resize(6);
+	std::vector<double> position;
+	position.resize(6);
 
-	//if(robot.GetRobotCartesianPosition(position)){
-	//	for_each(position.begin(), position.end(), print);
+	if(robot.GetRobotCartesianPosition(position)){
+		for_each(position.begin(), position.end(), print);
 		std::cout <<"\n";
-		res->x = 1; //position[0];
-		res->y = 2; //position[1];
-		res->z = 3; //position[2];
-		res->theta_x  = 4; //position[3];
-		res->theta_y  = 5; //position[4];
-		res->theta_z  = 6; //position[5];
-	//}else
-	//	std::cout <<"wrong\n";
+		res->x = position[0];
+		res->y = position[1];
+		res->z = position[2];
+		res->theta_x  = position[3];
+		res->theta_y  = position[4];
+		res->theta_z  = position[5];
+	}else
+		std::cout <<"wrong\n";
 
+	robot.Logoff();
 	return 0;
+}
+
+int MoveJoints(staubli::command::Request  *req,
+			   staubli::command::Response *res)
+{
+	TX60L robot;
+    robot.Login("http://128.59.20.63:5653/", "default", ""); //ANTIGUA
+	//robot.Login("http://128.59.19.61:5653/", "default", ""); //Thomas' computer
+	robot.Power(true);
+	ROS_INFO("Staubli is connected !!.");
+
+	//Move joints
+	std::vector<double> j;
+	j[0]=req->joint1;
+	j[1]=req->joint2;
+	j[2]=req->joint3;
+	j[3]=req->joint4;
+	j[4]=req->joint5;
+	j[5]=req->joint6;
+	robot.ResetMotion();
+	robot.MoveJoints(j);
+
+	//Get Current joints
+	std::vector<double> current_joints;
+	current_joints.resize(6);
+
+	if(robot.GetRobotJoints(current_joints)){
+		for_each(current_joints.begin(), current_joints.end(), print);
+		std::cout <<"\n";
+		res->joint1 = current_joints[0];
+		res->joint2 = current_joints[1];
+		res->joint3 = current_joints[2];
+		res->joint4  = current_joints[3];
+		res->joint5  = current_joints[4];
+		res->joint6  = current_joints[5];
+	}else
+		std::cout <<"wrong\n";
+
+	robot.Logoff();
 }
 
 bool commandhandler(staubli::command::Request  &req,
@@ -50,6 +91,7 @@ bool commandhandler(staubli::command::Request  &req,
 	ROS_INFO("Command #%d.",req.command_number);
 	switch(req.command_number){
 		case 1 : GetPosition(&res); break;
+		case 2 : MoveJoints(&req,&res);break;
 		default: break;
 	}
 	return true;
